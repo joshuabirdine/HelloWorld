cd9587ae526e5ab94028826f1337f69434128cd4
joshuabirdine <joshbird5@gmail.com>
2019-05-22 22:52:19 -0700

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+print('--------------------------------')
+print('              Hello App')
+print('--------------------------------')
+print()
+
+
+
+# asks for input from user and saves it as a variable
+user_text = input('What is your name?') 
+# creates another variable and adds user_text variable to finish out sentence
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
